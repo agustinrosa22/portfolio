1f7498f3298f16fd09502fc29ac43f2ba19c9abb
agustinrosa22 <agustinrosa1234@gmail.com>
2023-09-13 23:51:35 -0300

    cssEffectFooter

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,21 +8,30 @@ import cvLogo from '../../Assets/cv.svg'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.social}>
-        <a  href="https://linkedin.com/in/agustin-rosa-37ab2a26a" target="_blank" rel="noopener noreferrer">
-          <img className={styles.logo} src={LinkedinLogo} alt="" />
-        </a>
-        <a  href="https://github.com/agustinrosa22" target="_blank" rel="noopener noreferrer">
-          <img className={styles.logo} src={githubLogo} alt="" />
-        </a>
-        <a href="https://drive.google.com/file/d/1ik042EJtndjdAtpb0u9cQYSDdBJWzymI/view?usp=drive_link" target="_blank">
-         <img className={styles.logo} src={cvLogo} alt="" />
-        </a>
-        <a  href="mailto:agustinrosa1234@gmail.com">
-          <img className={styles.logo} src={gmailLogo} alt="" />
-        </a>
+  <div className={styles.social}>
+    <a href="https://linkedin.com/in/agustin-rosa-37ab2a26a" target="_blank" rel="noopener noreferrer">
+      <div className={styles.logoContainer}>
+        <img className={styles.logo} src={LinkedinLogo} alt="" />
       </div>
-    </footer>
+    </a>
+    <a href="https://github.com/agustinrosa22" target="_blank" rel="noopener noreferrer">
+      <div className={styles.logoContainer}>
+        <img className={styles.logo} src={githubLogo} alt="" />
+      </div>
+    </a>
+    <a href="https://drive.google.com/file/d/1ik042EJtndjdAtpb0u9cQYSDdBJWzymI/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+      <div className={styles.logoContainer}>
+        <img className={styles.logo} src={cvLogo} alt="" />
+      </div>
+    </a>
+    <a href="mailto:agustinrosa1234@gmail.com" target="_blank" rel="noopener noreferrer">
+      <div className={styles.logoContainer}>
+        <img className={styles.logo} src={gmailLogo} alt="" />
+      </div>
+    </a>
+  </div>
+</footer>
+
   );
 };
 
